5aa980c9ba11f62b9975d8bbab520c087f2bcf16
LOIC HAMERSCHMIDT <130869756+LO-HAMER@users.noreply.github.com>
2024-02-07 23:19:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 const team = {
-    _players: [
-    {firstName: 'JAMES', lastName: 'BOND', age: 22 },
-    {firstName: 'PETER', lastName: 'PAN', age: 25 },{firstName: 'Harry', lastName: 'POTER', age: 20 }
+  _players: [
+  {firstName: 'JAMES', lastName: 'BOND', age: 22 },
+  {firstName: 'PETER', lastName: 'PAN', age: 25 },{firstName: 'Harry', lastName: 'POTER', age: 20 }
+  ],
+  _games: [
+    {opponent: 'madrid', teamPoints: '45', opponentPoints: '30'},
+    {opponent: 'barcelone', teamPoints: '62', opponentPoints: '50'},{opponent: 'rome', teamPoints: '78', opponentPoints: '35'}
     ],
-    _games: [
-      {opponent: 'madrid', teamPoints: '45', opponentPoints: '30'},
-      {opponent: 'barcelone', teamPoints: '62', opponentPoints: '50'},{opponent: 'rome', teamPoints: '78', opponentPoints: '35'}
-      ],
-  
-    get players () {
-        { 
-         return this._players
-         }
-    },
-  
-    get games () {
-      {
-        return this._games
-      }
-    }
-  
-  };
-  
-  
-  
+
+  get players () {  
+       return this._players;   
+  },
+
+  get games () {
+      return this._games;
+  },
+ 
+ addPlayer(newFirstName, newLastName, newAge){
+let players = {
+  firstName: newFirstName,
+  lastName: newLastName,
+  age: newAge
+};
+this.players.push(players);
+ } 
+};
+
+team.addPlayer('Bugs', 'Bunny', 76);
+console.log(team.players)
